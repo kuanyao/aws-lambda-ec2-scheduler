cb2294410ab8a81b53a9b08d6c1a682d98f6d619
Kuan Yao <kuanyao@amazon.com>
2016-08-14 16:39:23 -0400

    running on aws console.

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
+'use strict';
+
+console.log('Loading function');
+
 var AWS = require('aws-sdk');
-
 var ec2 = new AWS.EC2({ "region": "us-east-1"});
-var dryrun = true;
 
-var params = {
-  Filters: [
-    { Name: "tag:for", Values: ["robotc"] }
-  ]
+exports.handler = (event, context, callback) => {
+    //console.log('Received event:', JSON.stringify(event, null, 2))
+    const message = event.Records[0].Sns.Message;
+    console.log('From SNS:', message);
+    
+    var options = JSON.parse(message);
+    var action = options.Action;
+    var params = { Filters: options.Filters };
+    
+    ec2.describeInstances(params)
+        .promise()
+        .then(function(data){
+            var reservations = data.Reservations,
+                instances,
+                instanceIds;
+
+            if (reservations && reservations.length) {
+                instances = data.Reservations[0].Instances;
+                instanceIds = instances.map(i => i.InstanceId);
+                if (action == "start") {
+                    return ec2.startInstances({InstanceIds: instanceIds}).promise();
+                } else if (action == "stop") {
+                    return ec2.stopInstances({InstanceIds: instanceIds}).promise();
+                }
+            }
+        })
+        .then(function(data){
+            if (data) {
+                console.log(data);
+            } else {
+                console.log("no instance has been found.");
+            }
+        })
+        .catch(function(error){
+            console.error(error);
+        });
 };
-ec2.describeInstances(params, function(err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
-  else {
-    instances = data.Reservations[0].Instances;
-    if (!instances.length) {
-      console.log('no instance found.');
-    } else {
-      instanceIds = instances.map(i => i.InstanceId);
-      ec2.startInstances({InstanceIds: instanceIds, DryRun: dryrun}, function(err, data){
-         if (!err) {
-            console.log(data);
-            console.log(instances.length + " instances have been started.");
-         } else {
-            console.log(err, err.stack);
-         }
-      });
-    }
-  }
-});
